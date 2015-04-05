fee4ff8864406845a9901371e1f1c73854448ac0
efendicevat <eminozcan@gmail.com>
2015-04-05 20:15:09 +0300

    log eklendi.

--- Tottoo-AppEngine/src/com/ege/tottoo/helper/PlayHelper.java
+++ Tottoo-AppEngine/src/com/ege/tottoo/helper/PlayHelper.java
@@ -1,19 +1,32 @@
 package com.ege.tottoo.helper;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.ege.tottoo.User;
 
 public class PlayHelper {
+	
+	private static final Logger log = Logger.getLogger(PlayHelper.class.getName());
+	
 	public static boolean isPlayable(User user,String identifier,int currentLevel,int currentTurn) {
 		boolean isPlayable = false;
+		log.log(Level.WARNING,"user.getIdentifier() : "+user.getIdentifier());
+		log.log(Level.WARNING,"identifier : "+identifier);
 		if(user.getIdentifier().equalsIgnoreCase(identifier))
 		{
+			log.log(Level.WARNING,"user.getCurrentLevel() : "+user.getCurrentLevel());
+			log.log(Level.WARNING,"currentLevel : "+currentLevel);
 			if(user.getCurrentLevel()==currentLevel)
 			{
+				log.log(Level.WARNING,"user.getCurrentTurn() : "+user.getCurrentTurn());
+				log.log(Level.WARNING,"currentTurn : "+currentTurn);
 				if(user.getCurrentTurn()==currentTurn) {
 					isPlayable = true;
 				}
 			}
 		}
+		log.log(Level.WARNING,"isPlayable : "+isPlayable);
 		return isPlayable;
 	}
 }
